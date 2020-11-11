b328abf4debfde0afdeaf3f6a033b4c2fec13d9f
sarah dakkak <sarah.dakkak1@gmail.com>
2020-11-11 16:07:21 -0500

    cs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+flats = Flat.create!([
+ {name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3},
+ {name: 'Pretty thing',
+   address: '45 Saud Gardens London W9 1DT',
+   description: 'A happy summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+   price_per_night: 83,
+   number_of_guests: 35},
+ {name: 'hrey thing',
+   address: '343 434r3f Gardens London W9 1DT',
+   description: 'A pretty summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+   price_per_night: 2,
+   number_of_guests: 45},
+{name: 'calisse thing',
+  address: '4354 434r3f fgtrgtr London W9 1DT',
+  description: 'A dsvf summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 58,
+  number_of_guests: 500}])
